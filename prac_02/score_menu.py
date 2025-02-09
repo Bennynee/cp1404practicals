afb4142adc8e7f5dbffbd62b6bf155338e02161b
Admin <minhtriet.nguyen@my.jcu.edu.au>
2025-02-09 20:38:41 +0800

    Refactor score program to use functions

---
+++ prac_02/score_menu.py
@@ -0,0 +1,47 @@
+MENU = """(G)et a valid score (must be 0-100 inclusive)
+(P)rint result 
+(S)how stars
+(Q)uit"""
+
+
+def main():
+    score = None
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            result = determine_score(score)
+            print(f"Result: {result}")
+        elif choice == "S":
+            show_stars(score)
+        else:
+            print("Invalid choice")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell.")
+
+
+def get_valid_score():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score. Please enter a score between 0 and 100.")
+        score = float(input("Enter score: "))
+    return score
+
+
+def determine_score(score):
+    if score > 90:
+        return "Excellent"
+    elif score > 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def show_stars(valid_score):
+    print("*" * int(valid_score))
+
+
+main()
