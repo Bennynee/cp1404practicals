e995b7f928a316aa2f698952ea4c66f569369072
Admin <minhtriet.nguyen@my.jcu.edu.au>
2025-02-16 15:57:59 +0800

    prac 3

---
+++ prac_03/files.py
@@ -0,0 +1,29 @@
+name = input("Enter your name: ")
+
+file = open("name.txt", "w")
+file.write(name)
+file.close()
+
+file = open("name.txt", "r")
+name = file.read().strip()
+file.close()
+
+print(f"Hi {name}!")
+
+with open("numbers.txt", "r") as file:
+    first_number = int(file.readline())
+    second_number = int(file.readline())
+
+result = first_number + second_number
+print(result)
+
+total = 0
+with open("numbers.txt", "r") as file:
+    for line in file:
+        total += int(line)
+
+print(total)
+
+
+
+
